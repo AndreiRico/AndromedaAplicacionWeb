29a95fad9787075d80eec4f5de2126f6cf10ef91
Joseph Valenzuela <josephdavid200320@gmail.com>
2022-11-18 18:56:23 -0500

    CRUD del módulo Afiliar Mascota

--- FrontendMascotaFeliz/src/app/modulos/afiliar-mascota/afiliar-mascota.module.ts
+++ FrontendMascotaFeliz/src/app/modulos/afiliar-mascota/afiliar-mascota.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AfiliarMascotaRoutingModule } from './afiliar-mascota-routing.module';
 import { AfiliarMascotaComponent } from './afiliar-mascota/afiliar-mascota.component';
 import { ContactenmeComponent } from './contactenme/contactenme.component';
+import { CrearMascotaComponent } from './afiliar-mascota/crear-mascota/crear-mascota.component';
+import { BuscarMascotaComponent } from './afiliar-mascota/buscar-mascota/buscar-mascota.component';
 
 
 @NgModule({
   declarations: [
     AfiliarMascotaComponent,
-    ContactenmeComponent
+    ContactenmeComponent,
+    CrearMascotaComponent,
+    BuscarMascotaComponent
   ],
   imports: [
     CommonModule,
